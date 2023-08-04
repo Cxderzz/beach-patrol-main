75b58ee45f9019aa8bf99e96e7afb1d662e6a1ea
Cxderzz <sorenjameskelly@gmail.com>
2023-08-04 19:13:46 +1000

    changes to readme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      text: "#071c0e",
+      textdark: "#ffffff",
+      background: "#ffffff",
+      primary: "#46ceb5",
+      secondary: "#b2d9eb",
+      accent: "#2c7fa5",
+    },
+
+    extend: {
+      fontFamily: {
+        rowdies: ["Rowdies", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
